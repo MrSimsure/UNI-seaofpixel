c18756755bb6354d2384d438257b0e43a15eae84
MrSimone <44972930+MrSimsure@users.noreply.github.com>
2018-12-07 00:10:16 +0100

    Update player.js

--- server/player.js
+++ server/player.js
@@ -1,8 +1,6 @@
-
 Player = function(name, id)
 {
     var self = Entity();
-
     self.id = id,
     self.name = name,
 
@@ -11,58 +9,48 @@ Player = function(name, id)
     self.pUp = false,
     self.pDown = false,
 
-    self.speed = 10
+    self.speed = 10,
 
-    targetX = -1;
-    targetY = -1;
+    targetX = -1,
+    targetY = -1
+ 
 
     self.updatePosition = function()
     {  
-    	if(self.targetX != -1)
-    	{
-    		if(self.targetX < self.x)
-    		{
-    			self.x -= self.speed;
-    		}
-    		else if(self.targetX > self.x)
-    		{
-    		self.x += self.speed;
-    		}
-    		else {self.targetX = -1;}
-    	}
-    	
-    	
-    	
-    	if(self.targetY != -1)
-    	{
-    		if(self.targetY < self.y)
-    		{
-    		self.y -= self.speed;
- 		   	}
-	    	else if(self.targetY > self.y)
-	    	{
-	    	self.y += self.speed;
-	    	}
-    		else {self.targetY = -1;}
-    	}
-    	
+         if(self.targetX != -1)
+         {
+             if(self.targetX < self.x)
+             { self.x -= self.speed;}
+             else if(self.targetX > self.x)
+             {self.x += self.speed;}
+             else {self.targetX = -1;}
+         }
+                 
+         if(self.targetY != -1)
+         {
+             if(self.targetY < self.y)
+             {self.y -= self.speed;}
+             else if(self.targetY > self.y)
+             {self.y += self.speed;}
+             else {self.targetY = -1;}
+         }
+
         if(self.pRight) {self.x += self.speed;}
         if(self.pLeft) {self.x -= self.speed;}
         if(self.pUp) {self.y -= self.speed;}
         if(self.pDown) {self.y += self.speed;}
     }
-
-    Player.list[id] = self;
+     Player.list[id] = self;
     return self;
 }
 
-Player.list = {};
+ Player.list = {};
 
-Player.onConnect = function(socket,name)
+
+ Player.onConnect = function(socket,name)
 {
     var player = Player(name,socket.id);
-
-    //quando ricevi un messaggio dal client
+     //quando ricevi un messaggio dal client
     socket.on("keyPress", function(data)
     {
         if(data.id == "left")
@@ -75,4 +63,32 @@ Player.onConnect = function(socket,name)
         {player.pUp = data.state;}
     
         else if(data.id == "down")
-        {player.pDown = d
+        {player.pDown = data.state;}
+    });
+}
+
+
+Player.onDisconnect = function(socket)
+{
+    delete Player.list[socket.id];
+}
+
+ 
+Player.update = function()
+{
+    var pack = [];
+     //aggiorna la posizione di ogni player ed inpacchetta i dati per inviarli
+    for(var i in Player.list)
+    {
+        var current = Player.list[i];
+        current.updatePosition();
+         pack.push
+        ({
+            id : current.id,
+            name : current.name,
+            x : current.x,
+            y : current.y
+        });
+    }
+     return pack;
+ } 
