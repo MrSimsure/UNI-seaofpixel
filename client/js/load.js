2412c6815830b21517b779e8ffb426499f41b93e
MrSimone <44972930+MrSimsure@users.noreply.github.com>
2018-12-13 17:55:09 +0100

    full-screen

--- client/js/load.js
+++ client/js/load.js
@@ -22,6 +22,23 @@ function loadStrip(name , frameNum ,width, height)
 }
 
 
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+
 
 sprPlayer = loadSprite("spr_player");
 sprWater = loadSprite("spr_water");
