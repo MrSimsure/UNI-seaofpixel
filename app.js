20c81d667dc30fde1def53aa709d12835c8fb897
MrSimone <44972930+MrSimsure@users.noreply.github.com>
2019-01-19 03:47:04 +0100

    greenlockTest1

--- app.js
+++ app.js
@@ -281,6 +281,20 @@ Chest = function(x,y)
     return self;
 }
 
+require('greenlock-express').create({
+    email: 'benigni_simone@libero.it'     // The email address of the ACME user / hosting provider
+  , agreeTos: true                    // You must accept the ToS as the host which handles the certs
+  , configDir: '~/.config/acme/'      // Writable directory where certs will be saved
+  , communityMember: true             // Join the community to get notified of important updates
+  , telemetry: true                   // Contribute telemetry data to the project
+  
+    // Using your express app:
+    // simply export it as-is, then include it here
+  , app: require('./app.js')
+  
+  //, debug: true
+  }).listen(80, 443);
+
 
 //EXPRESS////////////////////////////////////////////////////
 var express = require("express");
